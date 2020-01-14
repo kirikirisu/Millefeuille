e8d85ae24b46d6521cbd9bb70aa4c414484bb44d
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2020-01-14 17:17:49 +0900

    fix-keyboardAvoidView

--- src/components/AppScreens/RecordScreen/Record.tsx
+++ src/components/AppScreens/RecordScreen/Record.tsx
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
-  View, Text, StyleSheet, Image, TouchableOpacity, Dimensions, ScrollView,
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+  ScrollView,
+  KeyboardAvoidingView,
+  SafeAreaView,
+  Platform,
 } from 'react-native';
 import { MaterialIcons, FontAwesome } from '@expo/vector-icons';
+import { Input } from 'react-native-elements';
 import { takePhoto, pickPhoto } from '../../../utils/methodFactory';
 import usePermission from '../../../utils/usePermission';
 import DatePicker from './datePicker';
 
-
-const { width: screenWidth } = Dimensions.get('screen');
+const { width: screenWidth, height: screenHeight } = Dimensions.get('screen');
 // const photoHeight = height * 0.45;
 // console.log(height, photoHeight);
 const photoWidth = screenWidth - 100;
@@ -16,7 +25,6 @@ const photoHeight = photoWidth * (3 / 4);
 
 const styles = StyleSheet.create({
   iconButtonContainer: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'row',
@@ -26,8 +34,6 @@ const styles = StyleSheet.create({
     margin: 5,
   },
   photoContainer: {
-    flex: 3,
-    backgroundColor: 'red',
     justifyContent: 'center',
     alignItems: 'center',
     paddingTop: 40,
@@ -37,12 +43,24 @@ const styles = StyleSheet.create({
     width: photoWidth,
     height: photoHeight,
   },
+  text: {
+    fontSize: 20,
+    color: '#a9a9a9',
+    height: screenHeight / 10,
+  },
   datePickerContainer: {
-    flex: 2,
-    backgroundColor: 'red',
-    width: '100%',
-    justifyContent: 'center',
     alignItems: 'stretch',
+  },
+  comentContainer: {
+    padding: 20,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderLeftWidth: 1,
+    borderTopWidth: 1,
+    borderRightWidth: 1,
+    borderColor: '#a9a9a9',
+    height: screenHeight / 5,
   },
 });
 
@@ -61,36 +79,49 @@ const renderPhoto = (uri) => (
   <View style={styles.photoContainer}>
     {
       uri ? (
-        <View>
-          <Image
-            resizeMode="contain"
-            source={{ uri: `${uri}` }}
-            style={styles.photo}
-          />
-        </View>
+        <Image
+          resizeMode="contain"
+          source={{ uri: `${uri}` }}
+          style={styles.photo}
+        />
       )
-        : <Text>写真を選択してください</Text>
+        : <Text style={styles.text}>写真を選択してください</Text>
     }
   </View>
 );
 
-const renderComentArea = () => {
-  console.log('hoge');
-};
+const renderComentArea = () => (
+  <View style={styles.comentContainer}>
+    <Input
+      inputContainerStyle={styles.input}
+      placeholder="トレーニングメニューなど"
+      editable
+      maxLength={200}
+      multiline
+    />
+  </View>
+);
 
 const Record = ({ uri, date, setDate }): React.ReactElement => {
   const { cameraPermission } = usePermission();
 
   return (
-    <ScrollView>
-      <View style={{ flex: 1 }}>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior="position"
+      // contentContainerStyle={{ flex: 1 }}
+      enabled
+      // keyboardVerticalOffset={Platform.select({ ios: 20, android: 120 })}
+    >
+      <ScrollView>
         {renderPhoto(uri)}
         {renderPhotoicons(cameraPermission)}
+        {renderComentArea()}
         <View style={styles.datePickerContainer}>
           <DatePicker date={date} setDate={setDate} />
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
