640fc6097d21a33f5a08713681c9735659e332ee
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2020-01-19 04:43:41 +0900

    ピッカーで取得した日ずけのフォーマットを修正

--- src/components/AppScreens/RecordScreen/Confirmation.tsx
+++ src/components/AppScreens/RecordScreen/Confirmation.tsx
@@ -17,20 +17,58 @@ type Props = {
   };
 }
 
-const renderTexts = (date, coment) => (
+// eslint-disable-next-line consistent-return
+const formatDate = (objDate): string => {
+  const YEAR = 3;
+  const DAY = 2;
+  const strDate = objDate.toString();
+  const element = strDate.split(' ');
+
+  switch (element[1]) {
+    case 'Jan':
+      return `${element[YEAR]}-${1}-${element[DAY]}`;
+    case 'Feb':
+      return `${element[YEAR]}-${2}-${element[DAY]}`;
+    case 'Mar':
+      return `${element[YEAR]}-${3}-${element[DAY]}`;
+    case 'Apr':
+      return `${element[YEAR]}-${4}-${element[DAY]}`;
+    case 'May':
+      return `${element[YEAR]}-${5}-${element[DAY]}`;
+    case 'Jun':
+      return `${element[YEAR]}-${6}-${element[DAY]}`;
+    case 'Jul':
+      return `${element[YEAR]}-${7}-${element[DAY]}`;
+    case 'Aug':
+      return `${element[YEAR]}-${8}-${element[DAY]}`;
+    case 'Sep':
+      return `${element[YEAR]}-${9}-${element[DAY]}`;
+    case 'Oct':
+      return `${element[YEAR]}-${10}-${element[DAY]}`;
+    case 'Nov':
+      return `${element[YEAR]}-${11}-${element[DAY]}`;
+    case 'Dec':
+      return `${element[YEAR]}-${12}-${element[DAY]}`;
+    default:
+  }
+};
+
+
+const renderTexts = (date, coment): React.ReactElement => (
   <View style={styles.container}>
     <View><Text>{coment}</Text></View>
+    <View><Text>{date}</Text></View>
   </View>
 );
 
 const Confirmation: React.FC<Props> = ({ uid, record }) => {
   const { uri, date, text } = record;
+  const formatedDate = formatDate(date);
 
-  console.log(typeof date);
   return (
     <View style={{ flex: 1 }}>
       {renderPhoto(uri)}
-      {renderTexts(date, text)}
+      {renderTexts(formatedDate, text)}
     </View>
   );
 };
