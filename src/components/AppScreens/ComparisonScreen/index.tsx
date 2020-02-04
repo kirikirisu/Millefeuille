7463412551630ba08f32af0dd875918625f0ec5c
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2020-02-05 00:54:36 +0900

    取得したデータを整形

--- src/components/AppScreens/ComparisonScreen/index.tsx
+++ src/components/AppScreens/ComparisonScreen/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationBottomTabScreenComponent } from 'react-navigation-tabs';
 import {
-  Text, View, StyleSheet, SectionList,
+  Text, View, StyleSheet, ScrollView, SectionListData,
 } from 'react-native';
+import recordThunkReducer from '../../../reducers/recordThunk';
 
 type Props = {
-  recordThunk: {
+  recordThunk: SectionListData<{
     coment: string;
     date: string;
     url: string;
-  };
+  }>;
 };
 
 const styles = StyleSheet.create({
@@ -18,14 +19,57 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+  },
+  header: {
+    fontSize: 32,
+  },
+  title: {
+    fontSize: 24,
+  },
+});
+
+function Item({ data }) {
+  console.log(data);
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{data.date}</Text>
+    </View>
+  );
+}
+
+const conversionForSection = (thunk) => new Promise((resolve) => {
+  const sectionData = [];
+  Object.keys(thunk).forEach((key, index) => {
+    sectionData.push({ title: key, data: thunk[key] });
+  });
+  resolve(sectionData);
 });
 
 const ComparisonScreen: React.FC<Props> = ({ recordThunk }) => {
-  console.log(typeof [recordThunk]);
+  console.log('mount');
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    conversionForSection(recordThunk).then((v) => { setData(v); });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <View />
-    </View>
+    <ScrollView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        {
+          data.map((item, index) => {
+            console.log(item.title);
+            return (
+              <Item key={item.title} data={item.data} />
+            );
+          })
+        }
+      </View>
+    </ScrollView>
   );
 };
 export default ComparisonScreen;
