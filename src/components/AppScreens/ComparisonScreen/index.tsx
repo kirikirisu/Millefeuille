a1569b3e195c9d4e5377227315f74e981393174b
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2020-02-05 11:37:10 +0900

    画像の表示

--- src/components/AppScreens/ComparisonScreen/index.tsx
+++ src/components/AppScreens/ComparisonScreen/index.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationBottomTabScreenComponent } from 'react-navigation-tabs';
 import {
-  Text, View, StyleSheet, ScrollView,
+  Text, View, StyleSheet, ScrollView, Dimensions, ImageBackground,
 } from 'react-native';
+
+const { height, width } = Dimensions.get('screen');
 
 type Props = {
   recordThunk: Record;
@@ -15,41 +17,49 @@ type Record = {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  imgContainer: {
+    width: width - 72,
+    height: width * 0.6,
+    marginHorizontal: 36,
+    paddingHorizontal: 36,
+    paddingVertical: 36 * 0.66,
+    marginVertical: 15,
+    borderRadius: 12,
   },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-  },
-  header: {
-    fontSize: 32,
-  },
-  title: {
-    fontSize: 24,
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 10,
+    elevation: 5,
   },
 });
 
-function Item({ data }) {
-  console.log(data);
+const Item = ({ title, data }) => {
+  console.log(title, data);
   return (
-    <View style={styles.item}>
-      <Text style={styles.title}>{data.date}</Text>
-    </View>
+    <ImageBackground
+      style={[styles.imgContainer, styles.shadow]}
+      imageStyle={{ borderRadius: 12 }}
+      source={{ uri: data.url }}
+    >
+      <View />
+    </ImageBackground>
   );
-}
+};
 
-// eslint-disable-next-line max-len
-const conversionForSection = (thunk): Promise<{ title: string; data: Record }[]> => new Promise((resolve) => {
-  const sectionData = [];
-  Object.keys(thunk).forEach((key, index) => {
-    sectionData.push({ title: key, data: thunk[key] });
-  });
-  resolve(sectionData);
-});
+const conversionForSection = (thunk): Promise<{ title: string; data: Record }[]> => new Promise(
+  (resolve) => {
+    const sectionData = [];
+    Object.keys(thunk).forEach((key, index) => {
+      sectionData.push({ title: key, data: thunk[key] });
+    });
+    resolve(sectionData);
+  },
+);
 
 const ComparisonScreen: React.FC<Props> = ({ recordThunk }) => {
   console.log('mount');
@@ -61,17 +71,15 @@ const ComparisonScreen: React.FC<Props> = ({ recordThunk }) => {
   }, []);
 
   return (
-    <ScrollView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        {
-          data.map((item, index) => {
-            console.log(item.title);
-            return (
-              <Item key={item.title} data={item.data} />
-            );
-          })
-        }
-      </View>
+    <ScrollView style={{ flex: 1, backgroundColor: 'rgb(255, 255, 255)' }}>
+      {
+        data.map((item, index) => {
+          const { title, data: recordData } = item;
+          return (
+            <Item key={title} title={title} data={recordData} />
+          );
+        })
+      }
     </ScrollView>
   );
 };
