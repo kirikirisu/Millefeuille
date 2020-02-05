9d82c7bb27c2d988783bd6afc7008784deebde7b
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2020-02-05 15:54:59 +0900

    日付とコメント追加

--- src/components/AppScreens/ComparisonScreen/index.tsx
+++ src/components/AppScreens/ComparisonScreen/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationBottomTabScreenComponent } from 'react-navigation-tabs';
 import {
-  Text, View, StyleSheet, ScrollView, Dimensions, ImageBackground,
+  Text, View, StyleSheet, ScrollView, Dimensions, ImageBackground, Platform,
 } from 'react-native';
 
 const { height, width } = Dimensions.get('screen');
@@ -23,8 +23,9 @@ const styles = StyleSheet.create({
     marginHorizontal: 36,
     paddingHorizontal: 36,
     paddingVertical: 36 * 0.66,
-    marginVertical: 15,
+    marginVertical: 20,
     borderRadius: 12,
+    marginBottom: (width * 0.6) * 0.35,
   },
   shadow: {
     shadowColor: '#000',
@@ -36,18 +37,45 @@ const styles = StyleSheet.create({
     shadowRadius: 10,
     elevation: 5,
   },
+  infor: {
+    flexDirection: 'column',
+    alignSelf: 'center',
+    position: 'absolute',
+    borderRadius: 12,
+    paddingHorizontal: 36,
+    paddingVertical: 10,
+    bottom: 20,
+    backgroundColor: '#a9a9a9',
+    width: width - (36 * 4),
+    height: (width * 0.6) * 0.35,
+  },
+  title: {
+    fontSize: 14 * 1.25,
+    fontWeight: '500',
+    paddingBottom: 5,
+  },
 });
 
 const Item = ({ title, data }) => {
   console.log(title, data);
   return (
-    <ImageBackground
-      style={[styles.imgContainer, styles.shadow]}
-      imageStyle={{ borderRadius: 12 }}
-      source={{ uri: data.url }}
-    >
-      <View />
-    </ImageBackground>
+    <View>
+      <ImageBackground
+        style={[styles.imgContainer, styles.shadow]}
+        imageStyle={{ borderRadius: 12 }}
+        source={{ uri: data.url }}
+      />
+      <View style={styles.infor}>
+        <ScrollView>
+          <Text style={styles.title}>
+            {data.date}
+          </Text>
+          <Text>
+            {data.coment}
+          </Text>
+        </ScrollView>
+      </View>
+    </View>
   );
 };
 
