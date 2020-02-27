ef8b9e237e043efbe1226a2272983cacebbabfc8
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2020-02-27 18:58:44 +0900

    add-toggleScreen

--- src/components/AppScreens/SettingScreen/index.tsx
+++ src/components/AppScreens/SettingScreen/index.tsx
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-native-elements';
 import {
   StyleSheet,
   View,
 } from 'react-native';
+import LottieView from 'lottie-react-native';
 import { logout } from '../../../utils/methodFactory';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  lottie: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
 
-const LogoutScreen: React.FC = () => (
-  <View style={styles.container}>
-    <Button
-      title="Logout"
-      type="outline"
-      onPress={() => logout()}
-    />
-  </View>
-);
+const LogoutScreen: React.FC = () => {
+  const [lottieScreen, setScreen] = useState(true);
+  const toggleScreen = () => setScreen(!lottieScreen);
+  return (
+    <View style={styles.container}>
+      {lottieScreen
+        ? (
+          <LottieView
+            style={styles.lottie}
+            // eslint-disable-next-line global-require
+            source={require('../../../../lotties/202-setting.json')}
+            autoPlay
+            loop={false}
+            speed={0.8}
+            onAnimationFinish={() => { toggleScreen(); }}
+          />
+        )
+        : <View /> }
+    </View>
+  );
+};
 
 export default LogoutScreen;
